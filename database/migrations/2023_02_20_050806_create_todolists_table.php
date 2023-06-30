ff7d2ed594e525dc5722439fdb21fdff07f989e5
Monica Padolina <91210534+padolinam@users.noreply.github.com>
2023-07-01 02:19:00 +0800

    Add files via upload

---
+++ database/migrations/2023_02_20_050806_create_todolists_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('todolists', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId(column: 'list_group_id')->constrained();
+            $table->string(column:'name');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('todolists');
+    }
+};
---
+++ database/migrations/2023_02_22_004655_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('todolist_id')->constrained();
+            $table->string('name');
+            $table->text('description');
+            $table->date('due_date')->nullable();
+            $table->time('due_time')->nullable();
+            $table->boolean('completed')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
